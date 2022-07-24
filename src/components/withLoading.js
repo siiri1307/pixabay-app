956335f2c2123216019fa8158ebfb52369026f9d
Siiri Saar <siiri.saar1307@gmail.com>
2022-07-24 19:05:22 +0300

    Change circular progress' color

--- src/components/withLoading.js
+++ src/components/withLoading.js
@@ -4,7 +4,7 @@ import { CircularProgress, Box } from "@mui/material"
 export const withLoading = (Component) => {
   const WithLoadingComponent = ({ isLoading, ...props }) => {
     if(!isLoading) return <Component {...props} />
-    return <Box sx={{display: "flex", justifyContent: "center"}}><CircularProgress color="inherit" /></Box>
+    return <Box sx={{display: "flex", justifyContent: "center"}}><CircularProgress /></Box>
   }
   return WithLoadingComponent
 }
